acf5756481b4111f8342da4c939efa3cc48e2031
Aleš Roubíček <ales@roubicek.name>
2019-07-14 14:48:03 +0200

    Added assets caching behaviour

--- website.ts
+++ website.ts
@@ -115,10 +115,38 @@ function createCloudFront(
           {
             eventType: "viewer-response",
             lambdaArn:
-              "arn:aws:lambda:us-east-1:661884430919:function:SecurityHeaders:6"
+              "arn:aws:lambda:us-east-1:661884430919:function:SecurityHeaders:7"
           }
         ]
       },
+      orderedCacheBehaviors: [
+        {
+          allowedMethods: ["GET", "HEAD", "OPTIONS"],
+          cachedMethods: ["GET", "HEAD", "OPTIONS"],
+          compress: true,
+          defaultTtl: 31536000,
+          forwardedValues: {
+            cookies: {
+              forward: "none"
+            },
+            headers: ["Origin"],
+            queryString: false
+          },
+          maxTtl: 31536000,
+          minTtl: 31536000,
+          pathPattern: "/assets/*",
+          targetOriginId: contentBucket.arn,
+          viewerProtocolPolicy: "redirect-to-https",
+          lambdaFunctionAssociations: [
+            // add lambda edge with cache headers for immutable assets
+            {
+              eventType: "viewer-response",
+              lambdaArn:
+                "arn:aws:lambda:us-east-1:661884430919:function:CacheHeaders:2"
+            }
+          ]
+        }
+      ],
       priceClass: "PriceClass_100",
       restrictions: {
         geoRestriction: {
